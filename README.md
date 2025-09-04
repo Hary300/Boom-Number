3a2e800d6527856cfafb1ce768e3b6467655588a
Haryadi Nugraha Putra <h.nugraha.p@gmail.com>
2025-09-05 02:07:49 +0700

    Done

---
+++ README.md
@@ -0,0 +1,5 @@
+# Link Excalidraw
+
+https://excalidraw.com/#json=_GfA7iYbHfFJvtEKORJUm,OZaGoJAXVDavj_nfFm5vdg
+
+[Excalidraw](./img/Untitled-2025-07-08-1349.excalidraw.svg)
---
+++ index.js
@@ -0,0 +1,37 @@
+'use strict';
+
+// Define the boom number
+let boom = Math.floor(Math.random() * 20) + 1;
+console.log('Boom number:', boom); // visible in console
+
+function checkGuess() {
+  let userGuess = parseInt(document.getElementById('guess').value);
+
+  if (userGuess < boom) {
+    document.getElementById('result').textContent = '⬆️ Going up';
+  } else if (userGuess > boom) {
+    document.getElementById('result').textContent = '⬇️ Going down';
+  } else {
+    document.getElementById('result').textContent =
+      "💥 BOOM! You're dead! The Boom Number: " + boom;
+
+    // reset game
+    boom = Math.floor(Math.random() * 20) + 1;
+    console.log('New boom number:', boom);
+  }
+}
+
+function checkFinal() {
+  let userFinal = parseInt(document.getElementById('finalGuess').value);
+
+  if (userFinal === boom) {
+    document.getElementById('finalResult').textContent =
+      '✅ Awesome, You just saved the world!';
+    // reset game
+    boom = Math.floor(Math.random() * 20) + 1;
+    console.log('Boom baru:', boom);
+  } else {
+    document.getElementById('finalResult').textContent =
+      '❌ Wrong!! try again!';
+  }
+}
